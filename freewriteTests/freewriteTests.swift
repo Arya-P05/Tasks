7ca61757900571bd96a67dd1c7f19a960a3f22b7
arya <jackrack040@gmail.com>
2026-05-07 11:33:40 -0400

    task rename

--- freewriteTests/freewriteTests.swift
+++ freewriteTests/freewriteTests.swift
@@ -7,7 +7,7 @@
 
 import Testing
 
-struct freewriteTests {
+struct TasksTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
