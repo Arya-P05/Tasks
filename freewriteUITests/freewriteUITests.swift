c719f9135c3be2fd63defebacd4eddabdc3f4c43
arya <jackrack040@gmail.com>
2026-05-07 11:33:42 -0400

    task rename

--- freewriteUITests/freewriteUITests.swift
+++ freewriteUITests/freewriteUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class freewriteUITests: XCTestCase {
+final class TasksUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
